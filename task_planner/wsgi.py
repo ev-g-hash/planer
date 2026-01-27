e15f3bfead9fd9dca53d8cd179b25cf5d0a2c3cd
Evgeny Pikhtulov <pihtulovevgeny@gmail.com>
2026-01-27 19:40:01 +0400

    push - изменения для статики

--- task_planner/wsgi.py
+++ task_planner/wsgi.py
@@ -5,7 +5,13 @@ WSGI config for task_planner project.
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
+
+# Определяем корень проекта
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_planner.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=str(BASE_DIR / 'staticfiles'))
